32e20283a5e3ddb47a4811938ebe0290acad727e
themattbook <themattbook@gmail.com>
2021-06-08 19:05:28 -0500

    First working commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto">
+    <div class="w-full md:w-5/6 mx-auto md:mt-10">
+      <Articles />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Articles from "./components/Articles.vue";
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap");
+body {
+  font-family: "Inter", sans-serif !important;
+}
+</style>
---
+++ src/components/Articles.vue
@@ -0,0 +1,112 @@
+<template>
+  <div v-for="article in news" v-bind:key="article">
+    <div
+      class="h-40 bg-gray-100 mt-3"
+      :style="{
+        backgroundImage: `url(${article.img})`,
+        backgroundSize: `cover`,
+      }"
+      v-if="article.img"
+    >
+      <div
+        class="flex items-end w-full h-full"
+        style="background-color: rgba(0, 0, 0, 0.6)"
+      >
+        <div class="px-4 py-2">
+          <div class="text-sm text-green-400 mb-2">{{ article.source }}</div>
+          <div class="mb-2">
+            <a
+              :href="article.url"
+              class="
+                font-semibold
+                leading-tight
+                text-xl text-gray-100
+                hover:text-gray-100
+                hover:underline
+              "
+              target="_blank"
+            >
+              {{ article.title }}
+            </a>
+          </div>
+          <div class="flex text-gray-200 text-sm">
+            <div class="pr-3">{{ article.published }}</div>
+            <div>{{ article.author }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full h-40 bg-gray-100 mt-3"
+      :style="{
+        background: `linear-gradient(90deg, rgba(36,1,97,1) 0%, rgba(78,9,121,1) 35%, rgba(0,212,255,1) 79%)`,
+      }"
+      v-else
+    >
+      <div
+        class="flex items-end h-full"
+        style="background-color: rgba(0, 0, 0, 0.6)"
+      >
+        <div class="px-4 py-2">
+          <div class="text-sm text-green-400 mb-2">{{ article.source }}</div>
+          <div class="mb-2">
+            <a
+              :href="article.url"
+              class="
+                font-semibold
+                leading-tight
+                text-xl text-gray-100
+                hover:text-gray-100
+                hover:underline
+              "
+              target="_blank"
+            >
+              {{ article.title }}
+            </a>
+          </div>
+          <div class="flex text-gray-200 text-sm">
+            <div class="pr-3">{{ article.published }}</div>
+            <div>{{ article.author }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Articles",
+  data() {
+    return {
+      message: "Hello",
+      news: [],
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=" +
+          import.meta.env.VITE_APIKEY
+      )
+      .then((response) => {
+        let date = new Date();
+        let articles = response.data.articles;
+        for (var i = 0; i < articles.length; i++) {
+          let articleData = {
+            title: articles[i].title,
+            author: articles[i].author,
+            desc: articles[i].description,
+            img: articles[i].urlToImage,
+            url: articles[i].url,
+            source: articles[i].source.name,
+            published: date.toDateString(articles[i].publishedAt),
+          };
+          this.news.push(articleData);
+        }
+      });
+  },
+};
+</script>
+<style></style>
